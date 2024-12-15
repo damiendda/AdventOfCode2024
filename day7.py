80bcb404ece6176bcdd5836e94213cccf997938b
Celophane <damien.dallerac@gmail.com>
2024-12-15 21:24:15 +0100

    Add solution for day7

--- day7.py
+++ day7.py
@@ -2,24 +2,35 @@ import sys
 
 arg_input = sys.argv[1]
 
+
 with open(arg_input, 'r') as f:
+    count = 0
     for l in f.readlines():
         eq = l.split(':')
         r = int(eq[0])
         op = list(map(int, eq[1].strip().split(' ')))
-        tmp = op[0]
+        vals = [[op[0]]]
+        i = 0
         print(f"{r} {op}")
         for o in op[1:]:
+            arr = set()
+            for val in vals[i]:
+                a = val+o
+                b = val*o
+                c = int(f'{val}{o}')
+                if a <= r:
+                    arr.add(a)
+                if b<= r:
+                    arr.add(b)
+                if c <= r:
+                    arr.add(c)
             if op[-1] == o:
-                if tmp * o == r:
-                    print(f"WIN({r}) {tmp} * {o}")
-                elif tmp + o == r:
-                    print(f"WIN({r}) {tmp} + {o}")
+                if r in arr:
+                    print("WIN")
+                    count += r
                 else:
-                    print(f"something wrong {tmp*o} {tmp+o}\n")
-            elif tmp * o > r:
-                print(f" {tmp} + {o}")
-                tmp += o
-            else:
-                print(f" {tmp} * {o}")
-                tmp *= o
+                    print("FALSE")
+            vals.append(arr)
+            i += 1
+
+    print(count)
